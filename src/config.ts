6bd984016477baad75601549a0d8205ffaff3e87
gian <giancarlorp1999@gmail.com>
2026-06-02 10:45:30 -0500

    fixing checkout.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const isProd = import.meta.env.VITE_ENV === "prod";
 
 export const API_URL = isProd
-  ? import.meta.env.VITE_API_URL_PROD
-  : import.meta.env.VITE_API_URL_LOCAL;
+  ? import.meta.env.VITE_API_URL_PROD   // https://muebleria-api.onrender.com
+  : import.meta.env.VITE_API_URL_LOCAL; // http://localhost:3001
 
 export const APP_URL = isProd
   ? import.meta.env.VITE_APP_URL_PROD
